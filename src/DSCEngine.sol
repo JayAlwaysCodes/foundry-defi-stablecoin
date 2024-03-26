96e9ea5cc5a8848db18ccccef5745d5d458bb558
jayalwayscodes <johnsonjardocs@gmail.com>
2024-03-26 04:09:26 +0100

    modified DSCEngine.sol

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -30,8 +30,15 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__TokenAddressAndPriceFeedAddressMustBeSameLength();
     error DSCEngine__NotAllowedToken();
     error DSCEngine__TransferFailed();
+    error DSCEngine__BreaksHealthFactor(uint256 healthFactor);
+    error DSCEngine__MintFailed();
 
     //State Variables
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant PRECISION = 1e18;
+    uint256 private constant LIQUIDATION_THRESHOLD = 50; //200% Over collateralized
+    uint256 private constant LIQUIDATION_PRECISION = 100;
+    uint256 private constant MIN_HEALTH_FACTOR = 1;
     mapping(address token => address priceFeed) private s_priceFeeds;
     mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
     mapping(address user => uint256 amountDscMinted) private s_DSCMinted;
@@ -105,9 +112,13 @@ contract DSCEngine is ReentrancyGuard {
     *@notice they must have more collateral value than the minimum threshold
     */
     function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
-        s_DSCMinted += amountDscToMint;
+        s_DSCMinted[msg.sender] += amountDscToMint;
         //if they minted too much
-        revertIfHealthFactorIsBroken(msg.sender);
+        _revertIfHealthFactorIsBroken(msg.sender);
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+        if(!minted){
+            revert DSCEngine__MintFailed();
+        }
     }
 
     function burnDsc() external {}
@@ -121,7 +132,7 @@ contract DSCEngine is ReentrancyGuard {
 
     function _getAccountInformation(address user) private view returns (uint256 totalDscMinted, uint256 collateralValueInUsd){
         totalDscMinted = s_DSCMinted[user];
-        collateralValueInUsd = getaccountCollateralValue(user);
+        collateralValueInUsd = getAccountCollateralValue(user);
     }
 
 
@@ -133,27 +144,37 @@ contract DSCEngine is ReentrancyGuard {
         //total DSC minted
         //total collateral VALUE
         (uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
+        uint256 collateralAdjustedForThreshold = (collateralValueInUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
+        return (collateralAdjustedForThreshold * PRECISION) / totalDscMinted;
     }
 
+    //1. check health factor (do they have enough collateral)
+    //2. Revert if they don't
     function _revertIfHealthFactorIsBroken(address user) internal view {
-        //1. check health factor (do they have enough collateral)
-        //2. Revert if they don't
+        uint256 userHealthFactor = _healthFactor(user);
+        if(userHealthFactor < MIN_HEALTH_FACTOR){
+            revert DSCEngine__BreaksHealthFactor(userHealthFactor);
+        }
 
     }
 
 
     //public & External View Functions
 
-    function getAccountCollateralValue(address user) public view returns(uint256){
+    function getAccountCollateralValue(address user) public view returns(uint256 totalCollateralValueInUsd){
         //loop through each collateral token, get the amount theyhave deposited, and map it to the price, to get the USD value
-        for(uint256 i = 0; i,s_collateralTokens.length; i++){
+        for(uint256 i = 0; i<s_collateralTokens.length; i++){
             address token = s_collateralTokens[i];
             uint256 amount = s_collateralDeposited[user][token];
+            totalCollateralValueInUsd += getUsdValue(token, amount);
             
         }
+        return totalCollateralValueInUsd;
     }
 
-    function getUsdValue(address token, uint256 amount) public view returns(uint256){
-
+    function getUsdValue(address token, uint256 amount) public view returns(uint256 ){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
     }
 }
