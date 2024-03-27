3930ed6baeea4558f5be98da90e41c78a7f5812a
jayalwayscodes <johnsonjardocs@gmail.com>
2024-03-27 21:58:18 +0100

    modified DSCEngine.sol

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -278,4 +278,10 @@ contract DSCEngine is ReentrancyGuard {
         (, int256 price,,,) = priceFeed.latestRoundData();
         return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
     }
+
+    
+
+    function getAccountInformation(address user) external view returns (uint256 totalDscMinted, uint256 collateralValueInUsd){
+        (totalDscMinted, collateralValueInUsd) = _getAccountInformation(user);
+    }
 }
