985cf86789cc4678a1d0b143881541aaba0f88b2
jayalwayscodes <johnsonjardocs@gmail.com>
2024-03-28 16:24:38 +0100

    wrote more tests

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -8,6 +8,7 @@ import {Test} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {MockV3Aggregator} from "../mocks/MockV3Aggregator.sol";
 
 contract Handler is Test {
     DSCEngine dsce;
@@ -15,6 +16,10 @@ contract Handler is Test {
 
     ERC20Mock weth;
     ERC20Mock wbtc;
+
+    uint256 public timesMintIsCalled;
+    address[] public userWithCollateralDeposited;
+    MockV3Aggregator public ethUsdPriceFeed;
 
     uint96 MAX_DEPOSIT_SIZE = type(uint96).max;
 
@@ -25,8 +30,34 @@ contract Handler is Test {
         address[] memory collateralTokens = dsce.getCollateralTokens();
         weth = ERC20Mock(collateralTokens[0]);
         wbtc = ERC20Mock(collateralTokens[1]);
+
+        ethUsdPriceFeed = MockV3Aggregator(dsce.getCollateralTokenPriceFeed(address(weth)));
     }
 
+    function mintDsc(uint256 amount, uint256 addressSeed) public{
+        if(userWithCollateralDeposited.length == 0){
+            return;
+        }
+        address sender = userWithCollateralDeposited[addressSeed % userWithCollateralDeposited.length];
+        
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = dsce.getAccountInformation(sender);
+
+        int256 maxDscToMint = (int256 (collateralValueInUsd) / 2) - int256(totalDscMinted);
+        if(maxDscToMint < 0){
+            return;
+
+        }
+        amount = bound(amount, 0, uint256(maxDscToMint));
+        if(amount == 0){
+            return;
+        }
+        vm.startPrank(sender);
+        dsce.mintDsc(amount);
+        vm.stopPrank();
+        timesMintIsCalled++;
+    }
+
+    // redeem collateral
     function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public{
         ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
         amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
@@ -36,8 +67,27 @@ contract Handler is Test {
         collateral.approve(address(dsce), amountCollateral);
         dsce.depositCollateral(address(collateral), amountCollateral);
         vm.stopPrank();
+        //double push
+        userWithCollateralDeposited.push(msg.sender);
     }
 
+    function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        uint256 maxCollateralToRedeem = dsce.getCollateralBalanceOfUser(address(collateral), msg.sender);
+        amountCollateral = bound(amountCollateral, 0, maxCollateralToRedeem);
+        if(amountCollateral == 0){
+            return;
+        }
+        dsce.redeemCollateral(address(collateral), amountCollateral);
+    }
+
+    //This breaks our invariant test suite!!!
+    // function updateCollateralPrice(uint96 newPrice) public{
+    //     int256 newPriceInt = int256(uint256(newPrice));
+    //     ethUsdPriceFeed.updateAnswer(newPriceInt);
+    // }
+
+    // Helper Functions
     function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock){
         if(collateralSeed % 2 ==0){
             return weth;
--- test/fuzz/Invariants.t.sol
+++ test/fuzz/Invariants.t.sol
@@ -6,7 +6,7 @@
 
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
@@ -28,7 +28,7 @@ contract Invariants is StdInvariant, Test {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
         (,,weth,wbtc,) = config.activeNetworkConfig();
-        //targetContract(address(dsce));
+        //targetContract(address(dsce)); 
         handler = new Handler(dsce, dsc);
         targetContract(address(handler));
 
@@ -44,6 +44,17 @@ contract Invariants is StdInvariant, Test {
         uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
         uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWbtcDeposited);
 
+        console.log("weth value: ", wethValue);
+        console.log("wbtc value: ", wbtcValue);
+        console.log("total Supply: ", totalSupply);
+        console.log("Times mint called: ", handler.timesMintIsCalled());
+
         assert(wethValue + wbtcValue >= totalSupply);
+    }
+
+    function invariant_gettersFunctionsShouldNotRevert() public view{
+        dsce.getLiquidationBonus();
+        dsce.getLiquidationThreshold();
+        dsce.getPrecision();
     }
 }
